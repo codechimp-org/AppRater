9b3f3ede7dd49c79cefbbcaa58da7b2b8ae48c99
Andrew Jackson <andrew@codechimp.org>
2013-09-25 19:11:49 +0100

    Refactoring to constants for preference strings

--- AppRater/src/org/codechimp/apprater/AppRater.java
+++ AppRater/src/org/codechimp/apprater/AppRater.java
@@ -9,51 +9,57 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 
 public class AppRater {
-	private final static int DAYS_UNTIL_PROMPT = 3;
-	private final static int LAUNCHES_UNTIL_PROMPT = 7;
+    // Preference Constants
+    private final static String PREF_NAME = "apprater";
+    private final static String PREF_LAUNCH_COUNT = "launch_count";
+    private final static String PREF_FIRST_LAUNCHED = "date_firstlaunch";
+    private final static String PREF_DONT_SHOW_AGAIN = "dontshowagain";
 
-	/**
-	 * Call this method at the end of your OnCreate method to determine whether
-	 * to show the rate prompt
-	 */
-	public static void app_launched(Context context) {
-		SharedPreferences prefs = context.getSharedPreferences("apprater", 0);
-		if (prefs.getBoolean("dontshowagain", false)) {
-			return;
-		}
+    private final static int DAYS_UNTIL_PROMPT = 3;
+    private final static int LAUNCHES_UNTIL_PROMPT = 7;
 
-		SharedPreferences.Editor editor = prefs.edit();
+    /**
+     * Call this method at the end of your OnCreate method to determine whether
+     * to show the rate prompt
+     */
+    public static void app_launched(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences(PREF_NAME, 0);
+        if (prefs.getBoolean(PREF_DONT_SHOW_AGAIN, false)) {
+            return;
+        }
 
-		// Increment launch counter
-		long launch_count = prefs.getLong("launch_count", 0) + 1;
-		editor.putLong("launch_count", launch_count);
+        SharedPreferences.Editor editor = prefs.edit();
 
-		// Get date of first launch
-		Long date_firstLaunch = prefs.getLong("date_firstlaunch", 0);
-		if (date_firstLaunch == 0) {
-			date_firstLaunch = System.currentTimeMillis();
-			editor.putLong("date_firstlaunch", date_firstLaunch);
-		}
+        // Increment launch counter
+        long launch_count = prefs.getLong(PREF_LAUNCH_COUNT, 0) + 1;
+        editor.putLong(PREF_LAUNCH_COUNT, launch_count);
 
-		// Wait for at least the number of launches and the number of days used
-		// until prompt
-		if (launch_count >= LAUNCHES_UNTIL_PROMPT) {
-			if (System.currentTimeMillis() >= date_firstLaunch
-					+ (DAYS_UNTIL_PROMPT * 24 * 60 * 60 * 1000)) {
-				showRateAlertDialog(context, editor);
-			}
-		}
+        // Get date of first launch
+        Long date_firstLaunch = prefs.getLong(PREF_FIRST_LAUNCHED, 0);
+        if (date_firstLaunch == 0) {
+            date_firstLaunch = System.currentTimeMillis();
+            editor.putLong(PREF_FIRST_LAUNCHED, date_firstLaunch);
+        }
 
-		editor.commit();
-	}
+        // Wait for at least the number of launches and the number of days used
+        // until prompt
+        if (launch_count >= LAUNCHES_UNTIL_PROMPT) {
+            if (System.currentTimeMillis() >= date_firstLaunch
+                    + (DAYS_UNTIL_PROMPT * 24 * 60 * 60 * 1000)) {
+                showRateAlertDialog(context, editor);
+            }
+        }
 
-	/**
-	 * Call this method directly if you want to force a rate prompt, useful for
-	 * testing purposes
-	 */
-	public static void showRateDialog(final Context context) {
-		showRateAlertDialog(context, null);
-	}
+        editor.commit();
+    }
+
+    /**
+     * Call this method directly if you want to force a rate prompt, useful for
+     * testing purposes
+     */
+    public static void showRateDialog(final Context context) {
+        showRateAlertDialog(context, null);
+    }
 
     /**
      * Call this method directly to go straight to play store listing for rating
@@ -65,55 +71,56 @@ public class AppRater {
     }
 
     /**
-	 * The meat of the library, actually shows the rate prompt dialog
-	 */
-	private static void showRateAlertDialog(final Context context, final SharedPreferences.Editor editor) {
-		Builder builder = new AlertDialog.Builder(context);
-		builder.setTitle(String.format(
-				context.getString(R.string.dialog_title),
-				context.getString(R.string.app_name)));
+     * The meat of the library, actually shows the rate prompt dialog
+     */
+    private static void showRateAlertDialog(final Context context, final SharedPreferences.Editor editor) {
+        Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle(String.format(
+                context.getString(R.string.dialog_title),
+                context.getString(R.string.app_name)));
 
-		builder.setMessage(String.format(
-				context.getString(R.string.rate_message),
-				context.getString(R.string.app_name)));
+        builder.setMessage(String.format(
+                context.getString(R.string.rate_message),
+                context.getString(R.string.app_name)));
 
-		builder.setPositiveButton(context.getString(R.string.rate),
-				new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int id) {
-						context.startActivity(new Intent(Intent.ACTION_VIEW, Uri
-								.parse("market://details?id="
-										+ context.getPackageName().toString())));
-						if (editor != null) {
-							editor.putBoolean("dontshowagain", true);
-							editor.commit();
-						}
-						
-						dialog.dismiss();			
-					}
-				});
+        builder.setPositiveButton(context.getString(R.string.rate),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        context.startActivity(new Intent(Intent.ACTION_VIEW, Uri
+                                .parse("market://details?id="
+                                        + context.getPackageName().toString())));
+                        if (editor != null) {
+                            editor.putBoolean(PREF_DONT_SHOW_AGAIN, true);
+                            editor.commit();
+                        }
 
-		builder.setNeutralButton(context.getString(R.string.later),
-				new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int id) {
-						if (editor != null) {
-							Long date_firstLaunch = System.currentTimeMillis();
-							editor.putLong("date_firstlaunch", date_firstLaunch);
-							editor.commit();
-						}
-						dialog.dismiss();
-					}
-				});
+                        dialog.dismiss();
+                    }
+                });
 
-		builder.setNegativeButton(context.getString(R.string.no_thanks),
-				new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int id) {
-						if (editor != null) {
-							editor.putBoolean("dontshowagain", true);
-							editor.commit();
-						}
-						dialog.dismiss();					}
-				});
-		
-		builder.show();
-	}
+        builder.setNeutralButton(context.getString(R.string.later),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        if (editor != null) {
+                            Long date_firstLaunch = System.currentTimeMillis();
+                            editor.putLong(PREF_FIRST_LAUNCHED, date_firstLaunch);
+                            editor.commit();
+                        }
+                        dialog.dismiss();
+                    }
+                });
+
+        builder.setNegativeButton(context.getString(R.string.no_thanks),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        if (editor != null) {
+                            editor.putBoolean(PREF_DONT_SHOW_AGAIN, true);
+                            editor.commit();
+                        }
+                        dialog.dismiss();
+                    }
+                });
+
+        builder.show();
+    }
 }
