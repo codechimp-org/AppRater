289314da870401e2d27fa7a7b162bd54ca9404c7
sbystrov <sbystrov@gmail.com>
2017-03-14 09:48:48 +0300

    Bug  #40 fixed: No positive (Rate Now) button on android 5.0 Russian Locale

--- AppRaterLibrary/src/main/java/org/codechimp/apprater/AppRater.java
+++ AppRaterLibrary/src/main/java/org/codechimp/apprater/AppRater.java
@@ -11,6 +11,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.util.Log;
+import android.view.Gravity;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class AppRater {
     // Preference Constants
@@ -301,7 +304,38 @@ public class AppRater {
                         }
                     });
         }
-        builder.show();
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
+                try {
+                    final Button buttonPositive = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+
+                    if (buttonPositive == null) {
+                        return;
+                    }
+
+                    LinearLayout linearLayout = (LinearLayout)buttonPositive.getParent();
+                    if (linearLayout == null) {
+                        return;
+                    }
+
+                    // Check positive button not fits in window
+                    boolean shouldUseVerticalLayout = false;
+                    if (buttonPositive.getLeft() + buttonPositive.getWidth() > linearLayout.getWidth()) {
+                        shouldUseVerticalLayout = true;
+                    }
+
+                    // Change layout orientation to vertical
+                    if (shouldUseVerticalLayout ) {
+                        linearLayout.setOrientation(LinearLayout.VERTICAL);
+                        linearLayout.setGravity(Gravity.END);
+                    }
+                } catch (Exception ignored) {
+                }
+            }
+        });
+        alertDialog.show();
     }
 
     @SuppressLint("NewApi")
